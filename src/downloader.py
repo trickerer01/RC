41a7a12a837d87c56e38ea42a06e8cbed33418f4
trickerer01 <onlysuffering+1@gmail.com>
2024-05-11 11:24:52 +0700

    Fix a bug where Ctrl+C interruption without `--keep-unfinished` flag could skip some files and not try to delete them

--- src/downloader.py
+++ src/downloader.py
@@ -365,18 +365,16 @@ class ImageDownloadWorker:
         await self._after_download()
 
     def at_interrupt(self) -> None:
-        if len(self._writes_active) > 0:
+        if len(self._downloads_active) > 0:
+            paths_active = sorted(ii.my_fullpath for ii in self._downloads_active if path.isfile(ii.my_fullpath))
             if Config.keep_unfinished:
-                unfinished_str = '\n '.join(f'{i + 1:d}) {s}' for i, s in enumerate(sorted(self._writes_active)))
-                Log.debug(f'at_interrupt: keeping {len(self._writes_active):d} unfinished file(s):\n {unfinished_str}')
+                unfinished_str = '\n '.join(f'{i + 1:d}) {s}' for i, s in enumerate(paths_active))
+                Log.debug(f'at_interrupt: keeping {len(paths_active):d} unfinished file(s):\n {unfinished_str}')
                 return
-            Log.debug(f'at_interrupt: cleaning {len(self._writes_active):d} unfinished file(s)...')
-            for unfinished in sorted(self._writes_active):
+            Log.debug(f'at_interrupt: cleaning {len(paths_active):d} unfinished file(s)...')
+            for unfinished in paths_active:
                 Log.debug(f'at_interrupt: trying to remove \'{unfinished}\'...')
-                if path.isfile(unfinished):
-                    remove(unfinished)
-                else:
-                    Log.debug(f'at_interrupt: file \'{unfinished}\' not found!')
+                remove(unfinished)
 
     def store_image_info(self, ii: ImageInfo) -> None:
         self._orig_count += 1
