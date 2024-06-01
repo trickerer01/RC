07eeb56c6daedbc9b12075630d8be0eac6b2f00b
trickerer01 <onlysuffering+1@gmail.com>
2024-06-01 18:05:13 +0700

    Add `-fslevelup` cmd argument to allow to adjust existing files scan base folder

--- src/path_util.py
+++ src/path_util.py
@@ -35,24 +35,35 @@ def scan_dest_folder() -> None:
     assert len(found_foldernames_dict.keys()) == 0
     if path.isdir(Config.dest_base):
         Log.info('Scanning dest folder...')
+        dest_base = path.abspath(Config.dest_base)
+        scan_depth = MAX_DEST_SCAN_SUB_DEPTH + Config.folder_scan_levelup
+        for _ in range(Config.folder_scan_levelup):
+            longpath, dirname = path.split(path.abspath(dest_base))
+            dest_base = normalize_path(longpath)
+            if dirname == '':
+                break
 
         def scan_folder(base_folder: str, level: int) -> None:
             for cname in listdir(base_folder):
                 fullpath = f'{base_folder}{cname}'
                 if path.isdir(fullpath):
                     fullpath = normalize_path(fullpath)
-                    if level < MAX_DEST_SCAN_SUB_DEPTH:
+                    if level < scan_depth:
                         found_foldernames_dict[fullpath] = list()
                         scan_folder(fullpath, level + 1)
+                    pass
                     found_foldernames_dict[base_folder].append(cname)
 
-        found_foldernames_dict[Config.dest_base] = list()
-        scan_folder(Config.dest_base, 0)
-        base_folders_count = len(found_foldernames_dict[Config.dest_base])
+        found_foldernames_dict[dest_base] = list()
+        scan_folder(dest_base, 0)
+        if Config.dest_base not in found_foldernames_dict:
+            found_foldernames_dict[Config.dest_base] = list()
+            scan_folder(Config.dest_base, 0)
+        base_folders_count = len(found_foldernames_dict[dest_base])
         total_files_count = sum(len(li) for li in found_foldernames_dict.values())
         Log.info(f'Found {base_folders_count:d} folder(s) in base and '
                  f'{total_files_count - base_folders_count:d} folder(s) in {len(found_foldernames_dict.keys()) - 1:d} subfolder(s) '
-                 f'(total folders: {total_files_count:d}, scan depth: {MAX_DEST_SCAN_SUB_DEPTH:d})')
+                 f'(total folders: {total_files_count:d}, scan depth: {scan_depth:d})')
 
 
 def folder_exists_in_folder(base_folder: str, idi: int) -> str:
