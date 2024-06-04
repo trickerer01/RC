084e5f09aa266022d7eaed780136279c0181eead
trickerer01 <onlysuffering+1@gmail.com>
2024-06-04 15:37:48 +0700

    Clarify positive and negative title/description extra tag matches

--- src/tagger.py
+++ src/tagger.py
@@ -299,11 +299,15 @@ def is_filtered_out_by_extra_tags(ai: AlbumInfo, tags_raw: List[str], extra_tags
                           LoggingFlags.EX_MISSING_TAGS)
         elif extag.startswith('-('):
             neg_matches = get_neg_and_group_matches(extag, tags_raw)
-            for conf, td in zip((Config.check_title_neg, Config.check_description_neg), (ai.title, ai.description)):
+            for conf, cn, td in zip(
+                (Config.check_title_neg, Config.check_description_neg),
+                ('TITL', 'DESC'),
+                (ai.title, ai.description)
+            ):
                 if conf and td:
                     for tmatch in match_text(extag, td, 'and'):
-                        tmatch_s = tmatch[:30]
-                        Log.trace(f'{sfol}Album {sname} has POST/DESC NEG match: {tmatch_s}')
+                        tmatch_s = tmatch[:50]
+                        Log.trace(f'{sfol}Album {sname} has {cn} NEG match: {tmatch_s}')
                         if tmatch_s not in neg_matches:
                             neg_matches.append(f'{tmatch_s}...')
             if neg_matches:
@@ -314,15 +318,17 @@ def is_filtered_out_by_extra_tags(ai: AlbumInfo, tags_raw: List[str], extra_tags
             negative = extag.startswith('-')
             my_extag = extag[1:] if negative else extag
             mtag = get_matching_tag(my_extag, tags_raw)
-            for conf, td in zip(
+            for conf, cn, np, td in zip(
                 (Config.check_title_pos, Config.check_title_neg, Config.check_description_pos, Config.check_description_neg),
+                ('TITL', 'TITL', 'DESC', 'DESC'),
+                ('NEG', 'POS', 'NEG', 'POS'),
                 (ai.title, ai.title, ai.description, ai.description)
             ):
                 if conf and td and not mtag:
                     mtag = match_text(my_extag, td)
                     if mtag:
                         mtag = f'{mtag[:50]}...'
-                        Log.trace(f'{sfol}Album {sname} has POST/DESC {"NEG" if negative else "POS"} match: {mtag}')
+                        Log.trace(f'{sfol}Album {sname} has {cn} {np} match: {mtag}')
             if mtag is not None and negative:
                 suc = False
                 Log.info(f'{sfol}Album {sname} contains excluded tag \'{mtag}\'. Skipped!',
