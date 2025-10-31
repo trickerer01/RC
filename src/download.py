trickerer01/RV

Another round of optimizations, cleanup, typo fixes and hinting

src/download.py
@@ -118,12 +118,12 @@ async def scan_video(vi: VideoInfo) -> DownloadResult:
         my_authors = [str(a.string).lower() for a in a_html.find('div', string='Artist').parent.find_all('span')]
     except Exception:
         Log.warn(f'Warning: cannot extract authors for {sname}.')
-        my_authors = []
+        my_authors: list[str] = []
     try:
         my_categories = [str(c.string).lower() for c in a_html.find('div', string='Categories').parent.find_all('span')]
     except Exception:
         Log.warn(f'Warning: cannot extract categories for {sname}.')
-        my_categories = []
+        my_categories: list[str] = []
     try:
         vi.uploader = str(a_html.find('div', string='Uploaded by').parent.find('a').get_text(strip=True)).lower()
     except Exception:
@@ -157,9 +157,9 @@ async def scan_video(vi: VideoInfo) -> DownloadResult:
             vi.comments = ('\n' + '\n\n'.join(comments_list) + '\n') if comments_list else ''
     if Config.check_uploader and vi.uploader and vi.uploader not in tags_raw:
         tags_raw.append(vi.uploader)
-    va_list = list(filter(lambda x: 'audio' in x or '(va)' in x or x.endswith('va'), my_authors))
+    va_list = [va for va in my_authors if any(_ in va for _ in ('audio', '(va)')) or va.endswith('va')]
     aucat_count = max(len(my_authors) - len(va_list), len(my_categories))
-    if aucat_count >= 6 and 'compilation' not in tags_raw and 'pmv' not in tags_raw:
+    if aucat_count >= 6 and not any(_ in tags_raw for _ in ('compilation', 'pmv')):
         Log.warn(f'{sname} has {len(my_authors):d} arts ({len(va_list):d} VA) and {len(my_categories):d} cats! Assuming compilation')
         tags_raw.append('compilation')
     if Config.solve_tag_conflicts:
@@ -177,12 +177,10 @@ async def scan_video(vi: VideoInfo) -> DownloadResult:
             except Exception:
                 pass
     if scenario:
-        matching_sq = scenario.get_matching_subquery(vi, tags_raw, score, rating)
-        utpalways_sq = scenario.get_utp_always_subquery() if tdiv is None else None
-        if matching_sq:
+        if matching_sq := scenario.get_matching_subquery(vi, tags_raw, score, rating):
             vi.subfolder = matching_sq.subfolder
             vi.quality = matching_sq.quality
-        elif utpalways_sq:
+        elif utpalways_sq := scenario.get_utp_always_subquery() if tdiv is None else None:
             vi.subfolder = utpalways_sq.subfolder
             vi.quality = utpalways_sq.quality
         else:
@@ -204,8 +202,7 @@ async def scan_video(vi: VideoInfo) -> DownloadResult:
         ddiv = a_html.find('div', string='Download')
         if ddiv is not None and ddiv.parent is not None:
             break
-        message_span = a_html.find('span', class_='message')
-        if message_span:
+        if message_span := a_html.find('span', class_='message'):
             Log.warn(f'Cannot find download section for {sname}, reason: \'{message_span.text}\', skipping...')
             return DownloadResult.FAIL_DELETED
         elif tries >= 5:
@@ -215,12 +212,7 @@ async def scan_video(vi: VideoInfo) -> DownloadResult:
         Log.debug(f'No download section for {sname}, retry #{tries:d}...')
         a_html = await fetch_html(f'{SITE_AJAX_REQUEST_VIDEO % vi.id}?popup_id={2 + tries + vi.id % 10:d}')
     links = ddiv.parent.find_all('a', class_='tag_item')
-    qualities = []
-    for lin in links:
-        try:
-            qualities.append(lin.text.replace('MP4 ', ''))
-        except Exception:
-            pass
+    qualities = tuple(lin.text.replace('MP4 ', '') for lin in links if lin.text)
     if vi.quality not in qualities:
         q_idx = 0
         Log.warn(f'Warning: cannot find quality \'{vi.quality}\' for {sname}, selecting \'{qualities[q_idx]}\'')
@@ -261,7 +253,7 @@ async def scan_video(vi: VideoInfo) -> DownloadResult:
 async def process_video(vi: VideoInfo) -> DownloadResult:
     vi.set_state(VideoInfo.State.DOWNLOAD_PENDING)
     res = await download_video(vi)
-    if res not in (DownloadResult.SUCCESS, DownloadResult.FAIL_SKIPPED, DownloadResult.FAIL_ALREADY_EXISTS):
+    if (1 << res) & DownloadResult.RESULT_MASK_CRITICAL:
         vi.set_state(VideoInfo.State.FAILED)
     return res
 
@@ -278,7 +270,8 @@ async def download_sceenshot(vi: VideoInfo, scr_num: int) -> DownloadResult:
         try:
             os.makedirs(my_folder)
         except Exception:
-            raise OSError(f'ERROR: Unable to create subfolder \'{my_folder}\'!')
+            Log.fatal(f'ERROR: Unable to create subfolder \'{my_folder}\'!')
+            raise
 
     try:
         async with await wrap_request('GET', my_link) as r:
@@ -307,7 +300,7 @@ async def download_sceenshot(vi: VideoInfo, scr_num: int) -> DownloadResult:
 async def download_sceenshots(vi: VideoInfo) -> DownloadResult:
     ret = DownloadResult.SUCCESS
     t: Task[DownloadResult]
-    for t in [get_running_loop().create_task(download_sceenshot(vi, scr_idx + 1)) for scr_idx in range(SCREENSHOTS_COUNT)]:
+    for t in (get_running_loop().create_task(download_sceenshot(vi, scr_idx + 1)) for scr_idx in range(SCREENSHOTS_COUNT)):
         res = await t
         if res not in (DownloadResult.SUCCESS, ret):
             ret = res
@@ -319,7 +312,7 @@ async def download_video(vi: VideoInfo) -> DownloadResult:
     retries = 0
     exact_quality = False
     ret = DownloadResult.SUCCESS
-    skip = Config.dm == DOWNLOAD_MODE_SKIP
+    skip = Config.download_mode == DOWNLOAD_MODE_SKIP
     status_checker = ThrottleChecker(vi)
 
     if skip is True:
@@ -365,7 +358,8 @@ async def download_video(vi: VideoInfo) -> DownloadResult:
             try:
                 os.makedirs(vi.my_folder)
             except Exception:
-                raise OSError(f'ERROR: Unable to create subfolder \'{vi.my_folder}\'!')
+                Log.fatal(f'ERROR: Unable to create subfolder \'{vi.my_folder}\'!')
+                raise
 
     while (not skip) and retries <= Config.retries:
         r = None
@@ -375,7 +369,7 @@ async def download_video(vi: VideoInfo) -> DownloadResult:
                 vi.set_flag(VideoInfo.Flags.ALREADY_EXISTED_EXACT)
             file_size = os.stat(vi.my_fullpath).st_size if file_exists else 0
 
-            if Config.dm == DOWNLOAD_MODE_TOUCH:
+            if Config.download_mode == DOWNLOAD_MODE_TOUCH:
                 if file_exists:
                     Log.info(f'{vi.sfsname} ({vi.quality}) already exists, size: {file_size:d} ({file_size / Mem.MB:.2f} Mb)')
                     vi.set_state(VideoInfo.State.DONE)
@@ -396,8 +390,8 @@ async def download_video(vi: VideoInfo) -> DownloadResult:
                     ckwargs.update(noproxy=True, allow_redirects=True)
                 ensure_conn_closed(r)
                 r = await wrap_request('GET', r.headers['Location'], **ckwargs, **hkwargs)
-            content_len = r.content_length or 0
-            content_range_s = r.headers.get('Content-Range', '/').split('/', 1)
+            content_len: int = r.content_length or 0
+            content_range_s = str(r.headers.get('Content-Range', '/')).split('/', 1)
             content_range = int(content_range_s[1]) if len(content_range_s) > 1 and content_range_s[1].isnumeric() else 1
             if (content_len == 0 or r.status == 416) and file_size >= content_range:
                 Log.warn(f'{vi.sfsname} ({vi.quality}) is already completed, size: {file_size:d} ({file_size / Mem.MB:.2f} Mb)')
@@ -431,8 +425,7 @@ async def download_video(vi: VideoInfo) -> DownloadResult:
             async with async_open(vi.my_fullpath, 'ab') as outf:
                 register_new_file(vi)
                 vi.set_flag(VideoInfo.Flags.FILE_WAS_CREATED)
-                if vi.dstart_time == 0:
-                    vi.dstart_time = get_elapsed_time_i()
+                vi.dstart_time = vi.dstart_time or get_elapsed_time_i()
                 async for chunk in r.content.iter_chunked(512 * Mem.KB):
                     await outf.write(chunk)
                     vi.bytes_written += len(chunk)

src/dthrottler.py
@@ -27,20 +27,20 @@ class ThrottleChecker:
         self._interrupted_speeds = deque[float](maxlen=3)
         self._speeds = deque[str](maxlen=5)
         self._response: ClientResponse | None = None
-        self._cheker: Task | None = None
+        self._checker: Task | None = None
 
     def prepare(self, response: ClientResponse, init_size: int) -> None:
         self._init_size = init_size
         self._response = response
 
     def run(self) -> None:
-        assert self._cheker is None
-        self._cheker = get_running_loop().create_task(self._check_video_download_status())
+        assert self._checker is None
+        self._checker = get_running_loop().create_task(self._check_video_download_status())
 
     def reset(self) -> None:
-        if self._cheker is not None:
-            self._cheker.cancel()
-            self._cheker = None
+        if self._checker is not None:
+            self._checker.cancel()
+            self._checker = None
         self._response = None
         self._speeds.clear()
 

src/idgaps.py
@@ -69,7 +69,7 @@ class IdGapsPredictor:
         self._streak += 1
 
     def count_existing(self, vi: VideoInfo) -> None:
-        if self._streak:
+        if self._streak and VideoScanWorker.get().has_found_any():
             skip_num = self._get_skip_num(vi)
             streak_is_complimentary = skip_num > 0 and (self._streak + 1) % skip_num == 0
             self._streak = 0

src/pages.py
@@ -61,7 +61,7 @@ async def main(args: Sequence[str]) -> None:
             return -1
         return 0
 
-    v_entries = []
+    v_entries: list[VideoInfo] = []
     maxpage = Config.end if Config.start == Config.end else 0
 
     pi = Config.start
@@ -104,7 +104,7 @@ async def main(args: Sequence[str]) -> None:
 
             if Config.get_maxid:
                 mirefs = a_html.find_all('a', class_=video_ref_class)
-                max_id = max(int(re_page_entry.search(str(_.get('href'))).group(1)) for _ in mirefs)
+                max_id = max(int(re_page_entry.search(_.get('href')).group(1)) for _ in mirefs)
                 Log.fatal(f'{APP_NAME}: {max_id:d}')
                 return
 
@@ -116,7 +116,7 @@ async def main(args: Sequence[str]) -> None:
                 orig_count = len(arefs)
                 for aref in arefs:
                     try:  # some post previews may be invalid
-                        cur_id = int(re_page_entry.search(str(aref.get('href'))).group(1))
+                        cur_id = int(re_page_entry.search(aref.get('href')).group(1))
                     except Exception:
                         continue
                     bound_res = check_id_bounds(cur_id)
@@ -166,7 +166,7 @@ async def main(args: Sequence[str]) -> None:
                     ))
 
             if pi - 1 > Config.start and lower_count == orig_count > 0 and not Config.scan_all_pages:
-                if maxpage == 0 or pi - 1 < maxpage:
+                if not (0 < maxpage <= pi - 1):
                     Log.info(f'Page {pi - 1:d} has all post ids below lower bound. Pages scan stopped!')
                 break
 

trickerer01/RC

(cherry picked from commit 7acc5f906a9329644fc120d83b8eb5b9357bc77b)

src/download.py
@@ -105,12 +105,12 @@ async def process_album(ai: AlbumInfo) -> DownloadResult:
         my_authors = [str(a.string).lower() for a in a_html.find('div', string='Artists:').parent.find_all('span')]
     except Exception:
         Log.warn(f'Warning: cannot extract authors for {sname}.')
-        my_authors = []
+        my_authors: list[str] = []
     try:
         my_categories = [str(c.string).lower() for c in a_html.find('div', string='Categories:').parent.find_all('span')]
     except Exception:
         Log.warn(f'Warning: cannot extract categories for {sname}.')
-        my_categories = []
+        my_categories: list[str] = []
     tdiv = a_html.find('div', string='Tags:')
     if tdiv is None:
         Log.info(f'Warning: album {sname} has no tags!')
@@ -155,11 +155,9 @@ async def process_album(ai: AlbumInfo) -> DownloadResult:
             except Exception:
                 pass
     if scenario:
-        matching_sq = scenario.get_matching_subquery(ai, tags_raw, score, rating)
-        utpalways_sq = scenario.get_utp_always_subquery() if tdiv is None else None
-        if matching_sq:
+        if matching_sq := scenario.get_matching_subquery(ai, tags_raw, score, rating):
             ai.subfolder = matching_sq.subfolder
-        elif utpalways_sq:
+        elif utpalways_sq := scenario.get_utp_always_subquery() if tdiv is None else None:
             ai.subfolder = utpalways_sq.subfolder
         else:
             Log.info(f'Info: unable to find matching or utp scenario subquery for {sname}, skipping...')
@@ -294,7 +292,7 @@ async def download_image(ii: ImageInfo) -> DownloadResult:
     sfilename = f'{ii.album.my_sfolder_full}{ii.filename}'
     retries = 0
     ret = DownloadResult.SUCCESS
-    skip = Config.dm == DOWNLOAD_MODE_SKIP and not ii.is_preview
+    skip = Config.download_mode == DOWNLOAD_MODE_SKIP and not ii.is_preview
     status_checker = ThrottleChecker(ii)
 
     if skip is True:
@@ -324,7 +322,7 @@ async def download_image(ii: ImageInfo) -> DownloadResult:
                 ii.set_flag(ImageInfo.Flags.ALREADY_EXISTED_EXACT)
             file_size = os.stat(ii.my_fullpath).st_size if file_exists else 0
 
-            if Config.dm == DOWNLOAD_MODE_TOUCH and not ii.is_preview:
+            if Config.download_mode == DOWNLOAD_MODE_TOUCH and not ii.is_preview:
                 if file_exists:
                     Log.info(f'{sname} already exists, size: {file_size:d} ({file_size / Mem.MB:.2f} Mb)')
                     ii.set_state(ImageInfo.State.DONE)
@@ -345,8 +343,8 @@ async def download_image(ii: ImageInfo) -> DownloadResult:
                     ckwargs.update(noproxy=True, allow_redirects=True)
                 ensure_conn_closed(r)
                 r = await wrap_request('GET', r.headers['Location'], **ckwargs, **hkwargs)
-            content_len = r.content_length or 0
-            content_range_s = r.headers.get('Content-Range', '/').split('/', 1)
+            content_len: int = r.content_length or 0
+            content_range_s = str(r.headers.get('Content-Range', '/')).split('/', 1)
             content_range = int(content_range_s[1]) if len(content_range_s) > 1 and content_range_s[1].isnumeric() else 1
             if (content_len == 0 or r.status == 416) and file_size >= content_range:
                 Log.warn(f'{sname} is already completed, size: {file_size:d} ({file_size / Mem.MB:.2f} Mb)')

src/dthrottler.py
@@ -27,20 +27,20 @@ class ThrottleChecker:
         self._interrupted_speeds = deque[float](maxlen=3)
         self._speeds = deque[str](maxlen=5)
         self._response: ClientResponse | None = None
-        self._cheker: Task | None = None
+        self._checker: Task | None = None
 
     def prepare(self, response: ClientResponse, init_size: int) -> None:
         self._init_size = init_size
         self._response = response
 
     def run(self) -> None:
-        assert self._cheker is None
-        self._cheker = get_running_loop().create_task(self._check_album_download_status())
+        assert self._checker is None
+        self._checker = get_running_loop().create_task(self._check_album_download_status())
 
     def reset(self) -> None:
-        if self._cheker is not None:
-            self._cheker.cancel()
-            self._cheker = None
+        if self._checker is not None:
+            self._checker.cancel()
+            self._checker = None
         self._response = None
         self._speeds.clear()
 

src/idgaps.py
@@ -66,7 +66,7 @@ class IdGapsPredictor:
         self._streak += 1
 
     def count_existing(self, ai: AlbumInfo) -> None:
-        if self._streak:
+        if self._streak and AlbumDownloadWorker.get().has_found_any():
             skip_num = self._get_skip_num(ai)
             streak_is_complimentary = skip_num > 0 and (self._streak + 1) % skip_num == 0
             self._streak = 0

src/pages.py
@@ -58,7 +58,7 @@ async def main(args: Sequence[str]) -> None:
             return -1
         return 0
 
-    v_entries = []
+    v_entries: list[AlbumInfo] = []
     maxpage = Config.end if Config.start == Config.end else 0
 
     pi = Config.start
@@ -101,7 +101,7 @@ async def main(args: Sequence[str]) -> None:
 
             if Config.get_maxid:
                 mirefs = a_html.find_all('a', class_=album_ref_class)
-                max_id = max(int(re_page_entry.search(str(_.get('href'))).group(1)) for _ in mirefs)
+                max_id = max(int(re_page_entry.search(_.get('href')).group(1)) for _ in mirefs)
                 Log.fatal(f'{APP_NAME}: {max_id:d}')
                 return
 
@@ -113,8 +113,7 @@ async def main(args: Sequence[str]) -> None:
             for aref in arefs:
                 href = str(aref.get('href'))
                 cur_id = int(re_page_entry.search(href).group(1))
-                bound_res = check_id_bounds(cur_id)
-                if bound_res != 0:
+                if bound_res := check_id_bounds(cur_id):
                     if bound_res < 0:
                         lower_count += 1
                     continue
@@ -128,7 +127,7 @@ async def main(args: Sequence[str]) -> None:
                 v_entries.append(AlbumInfo(cur_id, my_utitle if use_utitle else my_title, preview_link=my_preview_link))
 
             if pi - 1 > Config.start and lower_count == orig_count > 0 and not Config.scan_all_pages:
-                if maxpage == 0 or pi - 1 < maxpage:
+                if not (0 < maxpage <= pi - 1):
                     Log.info(f'Page {pi - 1:d} has all post ids below lower bound. Pages scan stopped!')
                 break
 