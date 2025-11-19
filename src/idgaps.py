d9bd2bba3463b1fdaff886d3f88765f7ba4159e9
trickerer01 <onlysuffering+1@gmail.com>
2025-11-19 08:41:30 +0700

    Only re-enable id gaps prediction after encountering 3 valid gaps again, up from 1
    (cherry picked from RV commit 84db43189666faf9392acf6ba1134126cfe712e1)

--- src/idgaps.py
+++ src/idgaps.py
@@ -11,7 +11,7 @@ from __future__ import annotations
 import itertools
 
 from config import Config
-from defs import IDGAP_PREDICTION_AUTO, IDGAP_PREDICTION_OFF, IntPair
+from defs import IDGAP_PREDICTION_AUTO, IDGAP_PREDICTION_OFF, PREDICTION_REENABLE_THRESHOLD, IntPair
 from downloader import AlbumDownloadWorker
 from iinfo import AlbumInfo
 from logger import Log
@@ -77,7 +77,7 @@ class IdGapsPredictor:
                 if streak_valid is False:
                     Log.error(f'Error: id gap predictor encountered unexpected valid post offset != {skip_num:d}. Disabling prediction!')
                     self._enabled = False
-            elif self._streaks_count and Config.predict_id_gaps == IDGAP_PREDICTION_AUTO:
+            elif self._streaks_count >= PREDICTION_REENABLE_THRESHOLD and Config.predict_id_gaps == IDGAP_PREDICTION_AUTO:
                 Log.warn(f'Warning: id gap predictor encountered another gap with post offset == {skip_num:d} re-enabling prediction!')
                 self._enabled = True
 
