trickerer01/RV

Convert into a proper module

rv/ids.py
@@ -6,32 +6,21 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 #
 
-import sys
-from asyncio import run as run_async
 from asyncio import sleep
-from collections.abc import Sequence
 
-from cmdargs import HelpPrintExitException, prepare_arglist
-from config import Config
-from defs import MIN_PYTHON_VERSION, MIN_PYTHON_VERSION_STR
-from download import at_interrupt, download
-from fetch_html import create_session
-from iinfo import VideoInfo
-from logger import Log
-from path_util import prefilter_existing_items
-from tagger import extract_id_or_group, extract_ids_from_links
-from util import at_startup
-from validators import find_and_resolve_config_conflicts
+from .config import Config
+from .download import download
+from .fetch_html import create_session
+from .iinfo import VideoInfo
+from .logger import Log
+from .path_util import prefilter_existing_items
+from .tagger import extract_id_or_group, extract_ids_from_links
+from .validators import find_and_resolve_config_conflicts
 
-__all__ = ('main_sync',)
+__all__ = ('process_ids',)
 
 
-async def main(args: Sequence[str]) -> None:
-    try:
-        prepare_arglist(args, False)
-    except HelpPrintExitException:
-        return
-
+async def process_ids() -> int:
     if Config.use_id_sequence:
         Config.id_sequence = extract_id_or_group(Config.extra_tags)
         if not Config.id_sequence:
@@ -77,32 +66,12 @@ async def main(args: Sequence[str]) -> None:
             Log.fatal(f'\nAll {orig_count:d} videos already exist. Aborted.')
         else:
             Log.fatal('\nNo videos found. Aborted.')
-        return
+        return -1
 
     async with create_session():
         await download(v_entries, True, removed_count)
 
-
-async def run_main(args: Sequence[str]) -> None:
-    await main(args)
-    await sleep(0.5)
-
-
-def main_sync(args: Sequence[str]) -> None:
-    assert sys.version_info >= MIN_PYTHON_VERSION, f'Minimum python version required is {MIN_PYTHON_VERSION_STR}!'
-
-    try:
-        run_async(run_main(args))
-    except (KeyboardInterrupt, SystemExit):
-        Log.warn('Warning: catched KeyboardInterrupt/SystemExit...')
-    finally:
-        at_interrupt()
-
-
-if __name__ == '__main__':
-    at_startup()
-    main_sync(sys.argv[1:])
-    exit(0)
+    return 0
 
 #
 #

rv/pages.py
@@ -6,16 +6,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 #
 
-import sys
-from asyncio import run as run_async
 from asyncio import sleep
-from collections.abc import Sequence
 
-from cmdargs import HelpPrintExitException, prepare_arglist
-from config import Config
-from defs import (
-    MIN_PYTHON_VERSION,
-    MIN_PYTHON_VERSION_STR,
+from .config import Config
+from .defs import (
     PREFIX,
     QUALITIES,
     SITE_AJAX_REQUEST_FAVOURITES_PAGE,
@@ -25,25 +19,20 @@ from defs import (
     SITE_AJAX_REQUEST_UPLOADER_PAGE,
     NamingFlags,
 )
-from download import at_interrupt, download
-from fetch_html import create_session, fetch_html
-from iinfo import VideoInfo
-from logger import Log
-from path_util import prefilter_existing_items
-from rex import re_page_entry, re_paginator, re_preview_entry
-from util import at_startup, get_time_seconds, has_naming_flag
-from validators import find_and_resolve_config_conflicts
-from version import APP_NAME
+from .download import download
+from .fetch_html import create_session, fetch_html
+from .iinfo import VideoInfo
+from .logger import Log
+from .path_util import prefilter_existing_items
+from .rex import re_page_entry, re_paginator, re_preview_entry
+from .util import get_time_seconds, has_naming_flag
+from .validators import find_and_resolve_config_conflicts
+from .version import APP_NAME
 
-__all__ = ('main_sync',)
+__all__ = ('process_pages',)
 
 
-async def main(args: Sequence[str]) -> None:
-    try:
-        prepare_arglist(args, True)
-    except HelpPrintExitException:
-        return
-
+async def process_pages() -> int:
     full_download = Config.quality != QUALITIES[-1]
     video_ref_class = 'th' if Config.playlist_name else 'th js-open-popup'
 
@@ -104,7 +93,7 @@ async def main(args: Sequence[str]) -> None:
                 mirefs = a_html.find_all('a', class_=video_ref_class)
                 max_id = max(int(re_page_entry.search(_.get('href')).group(1)) for _ in mirefs)
                 Log.fatal(f'{APP_NAME}: {max_id:d}')
-                return
+                return 0
 
             Log.info(f'page {pi - 1:d}...{" (this is the last page!)" if (0 < maxpage == pi - 1) else ""}')
 
@@ -179,31 +168,11 @@ async def main(args: Sequence[str]) -> None:
                 Log.fatal(f'\nAll {orig_count:d} videos already exist. Aborted.')
             else:
                 Log.fatal('\nNo videos found. Aborted.')
-            return
+            return -1
 
         await download(v_entries, full_download, removed_count)
 
-
-async def run_main(args: Sequence[str]) -> None:
-    await main(args)
-    await sleep(0.5)
-
-
-def main_sync(args: Sequence[str]) -> None:
-    assert sys.version_info >= MIN_PYTHON_VERSION, f'Minimum python version required is {MIN_PYTHON_VERSION_STR}!'
-
-    try:
-        run_async(run_main(args))
-    except (KeyboardInterrupt, SystemExit):
-        Log.warn('Warning: catched KeyboardInterrupt/SystemExit...')
-    finally:
-        at_interrupt()
-
-
-if __name__ == '__main__':
-    at_startup()
-    main_sync(sys.argv[1:])
-    exit(0)
+    return 0
 
 #
 #

rv/tagger.py
@@ -11,8 +11,8 @@ import os
 from collections.abc import Callable, Collection, Iterable, MutableSequence
 from typing import TypeAlias
 
-from config import Config
-from defs import (
+from .config import Config
+from .defs import (
     FILE_LOC_ARTS,
     FILE_LOC_CATS,
     FILE_LOC_PLAS,
@@ -24,9 +24,9 @@ from defs import (
     TAGS_CONCAT_CHAR,
     UTF8,
 )
-from iinfo import VideoInfo
-from logger import Log
-from rex import (
+from .iinfo import VideoInfo
+from .logger import Log
+from .rex import (
     prepare_regex_fullmatch,
     re_bracketed_tag,
     re_idval,
@@ -42,7 +42,7 @@ from rex import (
     re_uscore_mult,
     re_wtag,
 )
-from util import assert_nonempty, normalize_path
+from .util import assert_nonempty, normalize_path
 
 __all__ = (
     'extract_id_or_group',

trickerer01/RC

(cherry picked from RV commit fcd691446fced09b39aec73ee3dea7c5ce1ab4fe)

rc/ids.py
@@ -6,32 +6,21 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 #
 
-import sys
-from asyncio import run as run_async
 from asyncio import sleep
-from collections.abc import Sequence
 
-from cmdargs import HelpPrintExitException, prepare_arglist
-from config import Config
-from defs import MIN_PYTHON_VERSION, MIN_PYTHON_VERSION_STR
-from download import at_interrupt, download
-from fetch_html import create_session
-from iinfo import AlbumInfo
-from logger import Log
-from path_util import scan_dest_folder
-from tagger import extract_id_or_group, extract_ids_from_links
-from util import at_startup
-from validators import find_and_resolve_config_conflicts
+from .config import Config
+from .download import download
+from .fetch_html import create_session
+from .iinfo import AlbumInfo
+from .logger import Log
+from .path_util import scan_dest_folder
+from .tagger import extract_id_or_group, extract_ids_from_links
+from .validators import find_and_resolve_config_conflicts
 
-__all__ = ('main_sync',)
+__all__ = ('process_ids',)
 
 
-async def main(args: Sequence[str]) -> None:
-    try:
-        prepare_arglist(args, False)
-    except HelpPrintExitException:
-        return
-
+async def process_ids() -> int:
     if Config.use_id_sequence:
         Config.id_sequence = extract_id_or_group(Config.extra_tags)
         if not Config.id_sequence:
@@ -70,34 +59,14 @@ async def main(args: Sequence[str]) -> None:
 
     if orig_count == 0:
         Log.fatal('\nNo albums found. Aborted.')
-        return
+        return -1
 
     scan_dest_folder()
 
     async with create_session():
         await download(v_entries, removed_count)
 
-
-async def run_main(args: Sequence[str]) -> None:
-    await main(args)
-    await sleep(0.5)
-
-
-def main_sync(args: Sequence[str]) -> None:
-    assert sys.version_info >= MIN_PYTHON_VERSION, f'Minimum python version required is {MIN_PYTHON_VERSION_STR}!'
-
-    try:
-        run_async(run_main(args))
-    except (KeyboardInterrupt, SystemExit):
-        Log.warn('Warning: catched KeyboardInterrupt/SystemExit...')
-    finally:
-        at_interrupt()
-
-
-if __name__ == '__main__':
-    at_startup()
-    main_sync(sys.argv[1:])
-    exit(0)
+    return 0
 
 #
 #

rc/pages.py
@@ -6,41 +6,30 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 #
 
-import sys
-from asyncio import run as run_async
 from asyncio import sleep
-from collections.abc import Sequence
 
-from cmdargs import HelpPrintExitException, prepare_arglist
-from config import Config
-from defs import (
-    MIN_PYTHON_VERSION,
-    MIN_PYTHON_VERSION_STR,
+from .config import Config
+from .defs import (
     SITE_AJAX_REQUEST_FAVOURITES_PAGE,
     SITE_AJAX_REQUEST_MODEL_PAGE,
     SITE_AJAX_REQUEST_SEARCH_PAGE,
     SITE_AJAX_REQUEST_UPLOADER_PAGE,
     NamingFlags,
 )
-from download import at_interrupt, download
-from fetch_html import create_session, fetch_html
-from iinfo import AlbumInfo
-from logger import Log
-from path_util import scan_dest_folder
-from rex import re_page_entry, re_paginator
-from util import at_startup, has_naming_flag
-from validators import find_and_resolve_config_conflicts
-from version import APP_NAME
+from .download import download
+from .fetch_html import create_session, fetch_html
+from .iinfo import AlbumInfo
+from .logger import Log
+from .path_util import scan_dest_folder
+from .rex import re_page_entry, re_paginator
+from .util import has_naming_flag
+from .validators import find_and_resolve_config_conflicts
+from .version import APP_NAME
 
-__all__ = ('main_sync',)
+__all__ = ('process_pages',)
 
 
-async def main(args: Sequence[str]) -> None:
-    try:
-        prepare_arglist(args, True)
-    except HelpPrintExitException:
-        return
-
+async def process_pages() -> int:
     album_ref_class = 'th'
 
     if find_and_resolve_config_conflicts() is True:
@@ -100,7 +89,7 @@ async def main(args: Sequence[str]) -> None:
                 mirefs = a_html.find_all('a', class_=album_ref_class)
                 max_id = max(int(re_page_entry.search(_.get('href')).group(1)) for _ in mirefs)
                 Log.fatal(f'{APP_NAME}: {max_id:d}')
-                return
+                return 0
 
             Log.info(f'page {pi - 1:d}...{" (this is the last page!)" if (0 < maxpage == pi - 1) else ""}')
 
@@ -149,7 +138,7 @@ async def main(args: Sequence[str]) -> None:
                 Log.fatal(f'\nAll {orig_count:d} albums already exist. Aborted.')
             else:
                 Log.fatal('\nNo albums found. Aborted.')
-            return
+            return -1
         elif removed_count > 0:
             Log.info(f'[Deduplicate] {removed_count:d} / {orig_count:d} albums were removed as duplicates!')
 
@@ -157,27 +146,7 @@ async def main(args: Sequence[str]) -> None:
 
         await download(v_entries, removed_count)
 
-
-async def run_main(args: Sequence[str]) -> None:
-    await main(args)
-    await sleep(0.5)
-
-
-def main_sync(args: Sequence[str]) -> None:
-    assert sys.version_info >= MIN_PYTHON_VERSION, f'Minimum python version required is {MIN_PYTHON_VERSION_STR}!'
-
-    try:
-        run_async(run_main(args))
-    except (KeyboardInterrupt, SystemExit):
-        Log.warn('Warning: catched KeyboardInterrupt/SystemExit...')
-    finally:
-        at_interrupt()
-
-
-if __name__ == '__main__':
-    at_startup()
-    main_sync(sys.argv[1:])
-    exit(0)
+    return 0
 
 #
 #

rc/tagger.py
@@ -10,8 +10,8 @@ import json
 import os
 from collections.abc import Callable, Collection, Iterable, MutableSequence
 
-from config import Config
-from defs import (
+from .config import Config
+from .defs import (
     FILE_LOC_ARTS,
     FILE_LOC_CATS,
     FILE_LOC_TAG_ALIASES,
@@ -22,9 +22,9 @@ from defs import (
     TAGS_CONCAT_CHAR,
     UTF8,
 )
-from iinfo import AlbumInfo
-from logger import Log
-from rex import (
+from .iinfo import AlbumInfo
+from .logger import Log
+from .rex import (
     prepare_regex_fullmatch,
     re_bracketed_tag,
     re_idval,
@@ -40,7 +40,7 @@ from rex import (
     re_uscore_mult,
     re_wtag,
 )
-from util import assert_nonempty, normalize_path
+from .util import assert_nonempty, normalize_path
 
 __all__ = (
     'extract_id_or_group',