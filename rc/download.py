59cd3ac7d599ba44a1f211c4c7da679c66bbf1c5
trickerer01 <onlysuffering+1@gmail.com>
2026-01-24 10:39:27 +0700

    Do not count temporary client-side connector errors towards retries count during download stage in addition to at fetch stage
    (cherry picked from RV commit e5ab7c7b93916ab520f107f5113fc82ec816be2e)

--- rc/download.py
+++ rc/download.py
@@ -13,7 +13,7 @@ import urllib.parse
 from asyncio import sleep
 
 from aiofile import async_open
-from aiohttp import ClientPayloadError
+from aiohttp import ClientConnectorError, ClientPayloadError
 
 from .config import Config
 from .defs import (
@@ -398,7 +398,7 @@ async def download_image(ii: ImageInfo) -> DownloadResult:
             break
         except Exception as e:
             Log.error(f'{ii.sname}: {sys.exc_info()[0]}: {sys.exc_info()[1]}')
-            if (r is None or r.status != 403) and isinstance(e, ClientPayloadError) is False:
+            if (r is None or r.status != 403) and not isinstance(e, (ClientPayloadError, ClientConnectorError)):
                 try_num += 1
                 Log.error(f'{sfilename}: error #{try_num:d}...')
             if r is not None and r.closed is False:
