d424cc5f8ea0d9decf03b59624987361fbace059
trickerer01 <onlysuffering+1@gmail.com>
2026-05-13 11:13:37 +0700

    Update artists/categories/tags lists

--- rc/tests.py
+++ rc/tests.py
@@ -176,13 +176,13 @@ class CmdTests(TestCase):
     def test_cmd_pages04(self):
         prepare_arglist(['pages',
                          '-search_tag', '6*,5???*', '-search_rule_tag', 'any',
-                         '-search_art', '*ikan', '-search_rule_art', 'any',
+                         '-search_art', '*man1*', '-search_rule_art', 'any',
                          '-search_cat', 'ali??_*', '-search_rule_cat', 'any',
                          # '-blacklist', 'a:6*9,c:*z,t:6g*,t:8*',
                          '-start', '3', '-pages', '2'])
         self.assertEqual('10838,11146,11950,2007,3364,4326,4867,9244,9363', Config.search_tags)
-        self.assertEqual('1338,4834,5129,5163', Config.search_arts)
-        self.assertEqual('315,685,979', Config.search_cats)
+        self.assertEqual('6864,6893', Config.search_arts)
+        self.assertEqual('1897,315,685,979', Config.search_cats)
         # self.assertEqual('model:25905,model:34361,cat:1277,cat:315,cat:3315,cat:557,tag:38580,tag:3966', Config.blacklist)
         self.assertEqual('any', Config.search_rule_tag)
         self.assertEqual('any', Config.search_rule_art)
